2a1b01bf95a8227b1c8c23f58681ab5537c8e1b6
nicklasgv <nvedeby@gmail.com>
2023-06-15 08:22:32 +0200

    <Added new sdcard cpp file

--- Arduino Blink/src/main.cpp
+++ Arduino Blink/src/main.cpp
@@ -1,14 +1,14 @@
-// #include <Arduino.h>
+#include <Arduino.h>
 
-// void setup() {
-//   // put your setup code here, to run once:
-//   pinMode(2, OUTPUT);
-// }
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+}
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   digitalWrite(2, HIGH);
-//   delay(1000);
-//   digitalWrite(2, LOW);
-//   delay(1000);
-// }
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
